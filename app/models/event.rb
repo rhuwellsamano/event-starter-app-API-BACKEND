4aadf2774e776f0b31c26c8e6d5939e8da6a989c
rhuwellsamano <rhuwell.samano@gmail.com>
2019-02-13 12:17:47 -0500

    model relations done

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,2 +1,5 @@
 class Event < ApplicationRecord
+  has_many :comments
+  has_many :bookmarks
+  has_many :users, through: :bookmarks
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,4 @@
 class User < ApplicationRecord
+  has_many :bookmarks
+  has_many :events, through: :bookmarks
 end
